92be5dd1f984af2fd1f2338f4e6d0522dc497d0f
harishabhishek <harish2@illinois.edu>
2015-03-03 14:06:29 -0600

    Cs 421 Mp4 initial commit

---
+++ mp4test.rkt
@@ -0,0 +1,18 @@
+#lang eopl
+
+
+;;;Lexical configuration for the problem
+(define lexical
+  '((whitespace (whitespace) skip)
+    (comment ("%" (arbno (not #\newline))) skip)
+    (identifier (letter (arbno (or letter digit))) symbol)
+    (number (digit (arbno digit)) number)))
+
+(define grammar
+  '((expression ( "let" (arbno ID "=" expression) "in" expression) let-expr)
+    (expression ( "letrec" (arbno ID "=" expression) "in" expression) letrec-expr)
+    (expression ( "proc" "(" (arbno ID) ")" expression) proc-exor)
+    (expression ( "(" expression (arbno expression) ")") exp-expr)
+    (expression ( "newref" "(" expression ")" ) newref-expr)
+    (expression (
+ 
